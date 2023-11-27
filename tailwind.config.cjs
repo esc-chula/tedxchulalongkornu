487c7886fb1ff21e357644f2da821468847c755d
betich <betich365@gmail.com>
2023-11-28 01:29:33 +0700

    feat: index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,47 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontSize: {
+        screen: [
+          "96px",
+          {
+            lineHeight: "1",
+            fontWeight: "700",
+          },
+        ],
+        heading: [
+          "61px",
+          {
+            lineHeight: "1",
+            fontWeight: "700",
+          },
+        ],
+        subhead: [
+          "49px",
+          {
+            lineHeight: "1",
+            fontWeight: "600",
+          },
+        ],
+        title: [
+          "31px",
+          {
+            fontWeight: "500",
+          },
+        ],
+        body: [
+          "24px",
+          {
+            fontWeight: "400",
+          },
+        ],
+        small: [
+          "16px",
+          {
+            fontWeight: "400",
+          },
+        ],
+      },
       colors: {
         TED: {
           red: "#FF2B29",
