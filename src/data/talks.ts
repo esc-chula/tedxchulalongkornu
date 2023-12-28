0680bd51d05ca76202dae7c203b4fd2f50e084bb
betich <betich365@gmail.com>
2023-12-28 15:28:14 +0700

    feat: add images

---
+++ src/data/talks.ts
@@ -0,0 +1,7 @@
+export const talks = {
+  "1": { name: "Event 1", url: "https://www.youtube.com/watch?v=xQ9pkQ3xGXk " },
+  "2": { name: "Event 2", url: "https://www.youtube.com/watch?v=GF8PL--F8Pc " },
+  "3": { name: "Event 3", url: "https://www.youtube.com/watch?v=sfDs1xNU3Ns" },
+  "4": { name: "Event 4", url: "https://www.youtube.com/watch?v=5gX6dDCOQAA " },
+  "5": { name: "Event 5", url: "https://www.youtube.com/watch?v=XLa_gJSbPsw" },
+};
